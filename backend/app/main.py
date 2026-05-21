b949c17572d1ebcf67e6613f43fb0fb34b283215
Urshitaa <133492187+urshitaa@users.noreply.github.com>
2026-05-22 00:03:18 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -71,6 +71,7 @@ def create_app() -> FastAPI:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[
+            "http://127.0.0.1:5173",
             "http://localhost:8080",
             "http://127.0.0.1:8080",
             "https://resume-ai-53gn.vercel.app",
