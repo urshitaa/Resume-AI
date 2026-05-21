2a3fb4db044898f9e9a04f8c410e03e4d1925d63
Urshitaa <133492187+urshitaa@users.noreply.github.com>
2026-05-21 23:37:39 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -73,6 +73,7 @@ def create_app() -> FastAPI:
         allow_origins=[
             "http://localhost:8080",
             "http://127.0.0.1:8080",
+            "https://resume-ai-53gn.vercel.app",
         ],
         allow_credentials=True,
         allow_methods=["*"],
